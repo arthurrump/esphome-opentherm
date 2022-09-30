b3860aa61b095dd888aac258830b38a63a3e0c2c
Arthur Rump <hello@arthurrump.com>
2022-09-30 11:41:37 +0200

    Add summary to compile script

--- compile_all.py
+++ compile_all.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 status = 0
+results = {}
 
 for file in os.listdir():
     if os.path.isfile(file) and file.endswith(".yaml"):
@@ -9,6 +10,14 @@ for file in os.listdir():
         res = os.system(f"esphome compile {file}")
         print(f"------- Finished compiling {file} with status {res} -------")
         status += res
+        results[file] = res
+
+print("======= Results =======")
+for file, res in results.items():
+    print(f"{'✅' if res == 0 else '❌'} {file}")
+    if res != 0:
+        print(f"  Status: {res}")
+print("=======================")
 
 if status != 0:
     sys.exit(1)
