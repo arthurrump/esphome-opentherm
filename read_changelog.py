3b612c513981668d253fc9af282d4f8e7ff3ab25
Arthur Rump <hello@arthurrump.com>
2022-10-06 15:00:44 +0200

    Try fix CI release logic

--- read_changelog.py
+++ read_changelog.py
@@ -5,12 +5,13 @@ if __name__ == "__main__":
     version = sys.argv[1]
     in_version = False
 
-    with open(CHANGELOG_FILE, "r", encoding = "utf-8") as f:
-        for line in f.readlines():
-            if in_version:
+    with open(CHANGELOG_FILE, "r", encoding = "utf-8") as changelog:
+        with open(CHANGELOG_FILE + ".tmp", "w", encoding = "utf-8") as changelog_tmp:
+            for line in changelog.readlines():
+                if in_version:
+                    if line.startswith("##"):
+                        break
+                    changelog_tmp.write(line)
                 if line.startswith("##"):
-                    break
-                print(line, end = None)
-            if line.startswith("##"):
-                match = VERSION_HEADER_PATTERN.match(line)
-                in_version = match and ("v" + match.group(1)) == version
+                    match = VERSION_HEADER_PATTERN.match(line)
+                    in_version = match and ("v" + match.group(1)) == version
